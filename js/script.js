bcc8d66e78ae128ad3264bc9124dd366443399dd
TyraelS <oshynkarenko1503@gmail.com>
2018-08-13 16:33:08 +0300

    Responsive sidebar

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", ()=>{
   let langBar = document.querySelector('.langToggle');
-  let list = document.querySelector('#langlist');
   let clicked = false;
-  console.log(langBar);
   langBar.onclick = ()=> {
     clicked?clicked=false:clicked=true;
     if(clicked){
@@ -12,6 +10,16 @@ document.addEventListener("DOMContentLoaded", ()=>{
       langlist.classList.remove('visible');
       document.querySelector('.fa-caret-up').classList.replace('fa-caret-up', 'fa-caret-down');
     }
-    
   }
+
+  let sidebar = document.querySelector('.sidebar');
+  let sidebarToggle = document.querySelector('.sidebarToggle');
+  let arrowToggle = document.querySelector('.fa-arrow-left');
+  sidebarToggle.onclick = () => {
+    sidebar.classList.add('visible');
+  }
+  arrowToggle.onclick = () => {
+    sidebar.classList.remove('visible');
+  }
+
 });
